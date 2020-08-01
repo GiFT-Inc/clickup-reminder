d49eaa21fa623a82fc6a6663a5a67474fd07cdc7
tatsuya <t2h5k0@gmail.com>
2020-08-01 19:06:23 +0900

    parse response to generate message

--- clickup.ts
+++ clickup.ts
@@ -13,15 +13,36 @@ const axiosConfig: AxiosRequestConfig = {
 const clickupClient = axios.create(axiosConfig)
 
 const teamId = process.env.TEAM_ID
+const locale = 'ja-JP'
 
 export const remindDelayedTasks = async (): Promise<void> => {
   const now = Date.now()
+  const params = {
+    subtasks: true,
+    due_date_lt: now,
+  }
   await clickupClient
-    .get(`/team/${teamId}/task`, {
-      params: { subtasks: true, due_date_lt: now },
-    })
+    .get(`/team/${teamId}/task`, { params })
     .then((res) => {
-      console.log(res)
+      if (res.status === 200) {
+        res.data.tasks.forEach((task) => {
+          // TODO: set space name
+          const dueDate = new Date(
+            parseInt(task.due_date, 10),
+          ).toLocaleDateString(locale)
+          const taskName = task.name
+          const taskUrl = task.url
+          const assignees = task.assignees
+            .map((assignee) => {
+              return assignee.username
+            })
+            .join(',')
+          const message = `Due Date: ${dueDate} - <${taskUrl}|${taskName}> (${assignees})`
+          console.info(message)
+        })
+      } else {
+        console.error(res)
+      }
     })
     .catch((err) => {
       console.error(err)
